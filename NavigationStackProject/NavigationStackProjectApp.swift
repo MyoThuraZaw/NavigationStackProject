370f008505ceda81e7f1a1c49c2ffb37f277b91d
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2023-01-11 18:16:44 +0630

    Initial Commit

---
+++ NavigationStackProject/NavigationStackProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackProjectApp.swift
+//  NavigationStackProject
+//
+//  Created by Myo Thura Zaw on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
